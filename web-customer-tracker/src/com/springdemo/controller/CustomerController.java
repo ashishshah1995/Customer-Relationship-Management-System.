787ffffbaace957044af210f0f01d55ea06be805
Ashish Shah <shahashish991@gmail.com>
2019-05-13 15:38:28 -0400

    CRM application

--- web-customer-tracker/src/com/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/springdemo/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package com.luv2code.springdemo.controller;
+package com.springdemo.controller;
 
 import java.util.List;
 
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.luv2code.springdemo.entity.Customer;
-import com.luv2code.springdemo.service.CustomerService;
+import com.springdemo.entity.Customer;
+import com.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
